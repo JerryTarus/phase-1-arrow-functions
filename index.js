f3f3d38397c65b60a81a26117aa8767945cb6be8
Jerry Tarus <jerrytarus@gmail.com>
2023-10-01 10:45:44 +0300

    Arrow functions done

---
+++ index.js
@@ -0,0 +1,14 @@
+const function divide()
+
+function divide() {
+    return 2000 / 100;
+  }
+  
+  square = (number) => {
+    return number * 2;
+  };
+  
+  add = (num1, num2) => {
+    return num1 + num2;
+  };
+  
